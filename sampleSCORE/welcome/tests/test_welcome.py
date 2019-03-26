6f08fb141a577205ef71bbd9f4d084fd5b9e5045
nanaones <nara03050@gmail.com>
2019-03-26 10:43:41 +0900

    edit welcomeSCORE's testcode

--- sampleSCORE/welcome/tests/test_welcome.py
+++ sampleSCORE/welcome/tests/test_welcome.py
@@ -1,13 +1,18 @@
 import os
 
-from iconsdk.builder.transaction_builder import DeployTransactionBuilder
+from iconsdk.builder.transaction_builder import DeployTransactionBuilder, CallTransactionBuilder
 from iconsdk.builder.call_builder import CallBuilder
 from iconsdk.icon_service import IconService
 from iconsdk.libs.in_memory_zip import gen_deploy_data_content
+
 from iconsdk.providers.http_provider import HTTPProvider
 from iconsdk.signed_transaction import SignedTransaction
 
+from iconservice import Address, json_loads
+
+
 from tbears.libs.icon_integrate_test import IconIntegrateTestBase, SCORE_INSTALL_ADDRESS
+from tbears.libs.icon_jsonrpc import IconJsonrpc
 
 DIR_PATH = os.path.abspath(os.path.dirname(__file__))
 
@@ -17,21 +22,22 @@ class TestWelcome(IconIntegrateTestBase):
     SCORE_PROJECT = os.path.abspath(os.path.join(DIR_PATH, '..'))
 
     def setUp(self):
-        self._score_root_path=".teststatedb"
         print("---------------------setup-------------------------")
         print(self.SCORE_PROJECT)
         super().setUp()
 
-        self.icon_service = None
+        # self.icon_service = None
         # if you want to send request to network, uncomment next line and set self.TEST_HTTP_ENDPOINT_URI_V3
-        # self.icon_service = IconService(HTTPProvider(self.TEST_HTTP_ENDPOINT_URI_V3))
+        self.icon_service = IconService(HTTPProvider(self.TEST_HTTP_ENDPOINT_URI_V3))
 
         print("---------------------[setups]wallet load-------------------------")
         self.test_wallet_two = self._wallet_array[0]
 
+
         # install SCORE
         print("---------------------[setups]SCORE Deploy-------------------------")
-        self._score_address = self._deploy_score(to = "cx0000000000000000000000000000000000000000")['scoreAddress']
+        # self._score_address = self._deploy_score(to = "cx0000000000000000000000000000000000000000")['scoreAddress']
+        self._score_address = self._deploy_score()['scoreAddress']
 
     def _deploy_score(self, to: str = SCORE_INSTALL_ADDRESS) -> dict:
         # Generates an instance of transaction for deploying SCORE.
@@ -43,8 +49,6 @@ class TestWelcome(IconIntegrateTestBase):
             .content_type("application/zip") \
             .content(gen_deploy_data_content(self.SCORE_PROJECT)) \
             .build()
-
-        print(transaction)
 
         # Returns the signed transaction object having a signature
         signed_transaction = SignedTransaction(transaction, self._test1)
@@ -58,13 +62,9 @@ class TestWelcome(IconIntegrateTestBase):
 
         return tx_result
 
-    def test_score_update(self):
-        # update SCORE
-        tx_result = self._deploy_score(self._score_address)
 
-        self.assertEqual(self._score_address, tx_result['scoreAddress'])
-
-    def test_call_hello(self):
+    def test_call_welcome(self):
+        print("----------------[test call]------------------------")
         # Generates a call instance using the CallBuilder
         call = CallBuilder().from_(self._test1.get_address()) \
             .to(self._score_address) \
@@ -73,10 +73,47 @@ class TestWelcome(IconIntegrateTestBase):
 
         # Sends the call request
         response = self.process_call(call, self.icon_service)
+        print(response)
 
-        self.assertEqual("welcome", response)
+    def _use_scrooge_(self, _to: Address, _amount: int):
+        transaction_use_scrooge = CallTransactionBuilder() \
+            .from_(self._test1.get_address()) \
+            .to(self._score_address) \
+            .step_limit(10_000_000) \
+            .nid(3) \
+            .nonce(100) \
+            .method("scrooge") \
+            .params({
+
+            "_to":_to,
+            "_amount":_amount
+
+        }) \
+            .build()
+
+        signed_transaction = SignedTransaction(transaction_use_scrooge, self._test1)
+        tx_result = self.process_transaction(signed_transaction, self.icon_service)
+        return tx_result
 
 
-Test = TestWelcome();
-Test.setUp()
-Test.test_call_hello()
+    def test_send_test(self):
+        print("----------------[test sendTx]------------------------")
+        print("----------------[sendTx] Balance ------------------------")
+
+
+        _balance_test1 = self.icon_service.get_balance(self._test1.address)
+        _balance_test2 = self.icon_service.get_balance(self.test_wallet_two.address)
+
+        print(f"before send icx from test1, test1's balance : {_balance_test1}")
+        print(f"before send icx from test2, test2's balance : {_balance_test2}")
+
+        print("----------------[sendTx] Balance check before sendtx ------------------------")
+
+        tx_result = self._use_scrooge_(_to=self.test_wallet_two.address, _amount=100_000)
+        print(tx_result)
+
+        self.assertTrue('status' in tx_result)
+        self.assertEqual(1, tx_result['status'])
+
+
+
